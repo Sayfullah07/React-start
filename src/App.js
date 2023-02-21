79372c8bf887f26e13a27005d592cc6bbfa003a0
Sayfullah07 <magomadovu95@gmail.com>
2023-02-21 12:07:46 +0300

    start-react

---
+++ src/App.js
@@ -0,0 +1,9 @@
+function App() {
+    return (
+        <div className="App">
+            <a className="App-link" href="https://grozmer.ru/turizm/istorija-goroda-groznogo-solzha-giala.html">Grozny</a>
+        </div>
+    );
+}
+
+export default App
